8950e415980ff58586144fb69fad6683445cde1d
sinus-x <14353790+sinus-x@users.noreply.github.com>
2022-01-30 21:53:57 +0100

    Move version declaration to prevent unwanted imports
    As cli.py had to be imported, grapher.* functions were imported, too.
    And as they required libraries like pygal, the installation aborted.

--- grapher/cli.py
+++ grapher/cli.py
@@ -7,19 +7,11 @@ from grapher.extract import run as run_extract
 from grapher.graph import register_parser as register_graph_options
 from grapher.graph import run as run_graph
 
-VERSION = "0.0.1"
-
 
 def main(args=sys.argv):
     parser = argparse.ArgumentParser(
         prog="pumpkin-grapher",
         allow_abbrev=False,
-    )
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version="%(prog)s " + VERSION,
     )
 
     subparsers = parser.add_subparsers(dest="command")
